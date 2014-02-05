6a7a8d179281e14e85a3f566e57fd98b4a2a643e
Jerome <loisel.jerome@gmail.com>
2014-02-05 11:12:40 +0100

    addToHead, addToTail operations

--- src/main/scala/fr/jloisel/scala/samples/MyList.scala
+++ src/main/scala/fr/jloisel/scala/samples/MyList.scala
@@ -6,6 +6,23 @@ trait MyList[+T] {
 	def isEmpty : Boolean
 	def head : T
 	def tail : MyList[T]
+	def addToHead[R >: T](value : R) : MyList[R]
+	def addToTail[R >: T](value : R) : MyList[R]
+}
+
+case object EmptyList extends MyList[Nothing] {
+	def isEmpty = true
+	def head = throw new NoSuchElementException("Nil.head")
+	def tail = throw new NoSuchElementException("Nil.tail")
+	def addToHead[R >: Nothing](value : R) : MyList[R] = this addToTail value
+	def addToTail[R >: Nothing](value : R) : MyList[R] = new Cell(value)
+}
+
+case class Cell[T](head: T, tail: MyList[T]) extends MyList[T] {
+	def this(head: T) = this(head, EmptyList)
+	def isEmpty = false
+	def addToHead[R >: T](value : R) : MyList[R] = new Cell(value, this)
+	def addToTail[R >: T](value : R) : MyList[R] = new Cell(head, tail addToTail value)
 }
 
 object MyList {
@@ -14,16 +31,9 @@ object MyList {
 		if(index == 0) list.head
 		nth(index - 1, list.tail)
 	}
-
-	def singleton[T](value: T) : MyList[T] = new Cell(value, EmptyList)
-}
-
-case object EmptyList extends MyList[Nothing] {
-	def isEmpty = true
-	def head = throw new NoSuchElementException("Nil.head")
-	def tail = throw new NoSuchElementException("Nil.tail")
-}
-
-case class Cell[T](val head: T, val tail: MyList[T]) extends MyList[T] {
-	def isEmpty = false
+	
+	def apply[T]() : MyList[T] = EmptyList
+	def apply[T](first: T) : MyList[T] = new Cell(first)
+	def apply[T](first: T, second: T) : MyList[T] = apply(first) addToTail second
+	def apply[T](first: T, second: T, third: T) : MyList[T] = apply(first, second) addToTail third
 }

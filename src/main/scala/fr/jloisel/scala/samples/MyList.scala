64f22784c7019a2caf96a1a6490875f660f91cb4
Jerome <loisel.jerome@gmail.com>
2014-02-05 12:22:19 +0100

    Remove covariance

--- src/main/scala/fr/jloisel/scala/samples/MyList.scala
+++ src/main/scala/fr/jloisel/scala/samples/MyList.scala
@@ -6,23 +6,23 @@ trait MyList[T] {
 	def isEmpty : Boolean
 	def head : T
 	def tail : MyList[T]
-	def addToHead[B <: T](value : B) : MyList[T]
-	def addToTail[B <: T](value : B) : MyList[T]
+	def addToHead(value : T) : MyList[T]
+	def addToTail(value : T) : MyList[T]
 }
 
 case class EmptyList[T] extends MyList[T] {
 	def isEmpty = true
 	def head = throw new NoSuchElementException("Nil.head")
 	def tail = throw new NoSuchElementException("Nil.tail")
-	def addToHead[B <: T](value : B) = this addToTail value
-	def addToTail[B <: T](value : B) = new Cell(value)
+	def addToHead(value: T) = this addToTail value
+	def addToTail(value: T) = new Cell(value)
 }
 
 case class Cell[T](head: T, tail: MyList[T]) extends MyList[T] {
 	def this(head: T) = this(head, new EmptyList)
 	def isEmpty = false
-	def addToHead[B <: T](value : B) = new Cell(value, new Cell(head, tail))
-	def addToTail[B <: T](value : B) = new Cell(head, tail addToTail value)
+	def addToHead(value : T) = new Cell(value, new Cell(head, tail))
+	def addToTail(value : T) = new Cell(head, tail addToTail value)
 }
 
 object MyList {

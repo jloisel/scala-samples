3661f7086946954cc75c339adeaa88580ee5a0dd
Jerome <loisel.jerome@gmail.com>
2014-02-05 12:14:28 +0100

    change the way generics are handled

--- src/main/scala/fr/jloisel/scala/samples/MyList.scala
+++ src/main/scala/fr/jloisel/scala/samples/MyList.scala
@@ -2,27 +2,27 @@ package fr.jloisel.scala.samples
 
 import java.util.NoSuchElementException
 
-trait MyList[+T] {
+trait MyList[T] {
 	def isEmpty : Boolean
 	def head : T
 	def tail : MyList[T]
-	def addToHead[R >: T](value : R) : MyList[R]
-	def addToTail[R >: T](value : R) : MyList[R]
+	def addToHead[B <: T](value : B) : MyList[T]
+	def addToTail[B <: T](value : B) : MyList[T]
 }
 
-case object EmptyList extends MyList[Nothing] {
+case class EmptyList[T] extends MyList[T] {
 	def isEmpty = true
 	def head = throw new NoSuchElementException("Nil.head")
 	def tail = throw new NoSuchElementException("Nil.tail")
-	def addToHead[R >: Nothing](value : R) : MyList[R] = this addToTail value
-	def addToTail[R >: Nothing](value : R) : MyList[R] = new Cell(value)
+	def addToHead[B <: T](value : B) = this addToTail value
+	def addToTail[B <: T](value : B) = new Cell(value)
 }
 
 case class Cell[T](head: T, tail: MyList[T]) extends MyList[T] {
-	def this(head: T) = this(head, EmptyList)
+	def this(head: T) = this(head, new EmptyList)
 	def isEmpty = false
-	def addToHead[R >: T](value : R) : MyList[R] = new Cell(value, this)
-	def addToTail[R >: T](value : R) : MyList[R] = new Cell(head, tail addToTail value)
+	def addToHead[B <: T](value : B) = new Cell(value, new Cell(head, tail))
+	def addToTail[B <: T](value : B) = new Cell(head, tail addToTail value)
 }
 
 object MyList {
@@ -32,7 +32,7 @@ object MyList {
 		nth(index - 1, list.tail)
 	}
 	
-	def apply[T]() : MyList[T] = EmptyList
+	def apply[T]() : MyList[T] = new EmptyList
 	def apply[T](first: T) : MyList[T] = new Cell(first)
 	def apply[T](first: T, second: T) : MyList[T] = apply(first) addToTail second
 	def apply[T](first: T, second: T, third: T) : MyList[T] = apply(first, second) addToTail third

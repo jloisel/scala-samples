f76467705259b16dd1d4cf9e26c9670ea40b8669
Jerome <loisel.jerome@gmail.com>
2014-02-05 12:23:53 +0100

    Fix Unit tests

--- src/test/scala/fr/jloisel/scala/samples/MyListSpec.scala
+++ src/test/scala/fr/jloisel/scala/samples/MyListSpec.scala
@@ -6,19 +6,19 @@ import org.scalatest.Matchers
 class MyListSpec extends FlatSpec with Matchers {
 
   "An empty list" should "be empty" in {
-    val list = Nil
+    val list = new EmptyList
     list.isEmpty should be (true)
   }
   
   it should "throw NoSuchElementException if an empty list head is retrieved" in {
-    val list = Nil
+    val list = new EmptyList
     a [NoSuchElementException] should be thrownBy {
       list.head
     }
   }
   
   it should "throw NoSuchElementException if an empty list tail is retrieved" in {
-    val list = Nil
+    val list = new EmptyList
     a [NoSuchElementException] should be thrownBy {
       list.tail
     }
